11f677a97513ce54babfc29f2904406105656658
Ronell B. Sicat <20956896+ronellsicat@users.noreply.github.com>
2022-06-15 17:53:12 -0400

    SegmentRoots and SegmentShoots looks good.

--- SegmentPlant.m
+++ SegmentPlant.m
@@ -1,10 +1,11 @@
-function binaryMask = ApplySegmentation(inputImage, maxIslandNumPixels)
-%ApplySegmentation Computes the binary mask for roots/shoots using color
-%thresholding combining results from filtering RGB, YCBCR, and LAB color
-%spaces.
+function binaryMask = SegmentPlant(inputImage, maxIslandNumPixels)
+%SegmentPlant Computes the binary mask for plants, i.e., combined
+%roots/shoots, in the image, using color thresholding in RGB, YCBCR, and
+%LAB color spaces.
 %   Histogram equalization is first applied to the image to enhance
 %   contrast. After segmentation, the border pixels and small islands of
 %   size maxIslandNumPixels.
+
     if nargin < 2
         maxIslandNumPixels = 50;
     end
---
+++ SegmentShoots.m
@@ -0,0 +1,36 @@
+function binaryMask = SegmentShoots(inputImage, maxIslandNumPixels)
+%SegmentPlant Computes the binary mask for plants, i.e., combined
+%roots/shoots, in the image, using color thresholding in RGB, YCBCR, and
+%LAB color spaces.
+%   Histogram equalization is first applied to the image to enhance
+%   contrast. After segmentation, the border pixels and small islands of
+%   size maxIslandNumPixels.
+
+    if nargin < 2
+        maxIslandNumPixels = 100;
+    end
+
+    plantMask = SegmentPlant(inputImage);
+
+    
+
+    imageHist = histeq(inputImage);
+    %imageHist = (inputImage);
+
+    inputImage = uint8(plantMask) .* inputImage;
+    imageHist = uint8(plantMask) .* imageHist;
+    
+    binaryMask = createMaskShootsHSVorig(inputImage);% & createMaskRootsLABhisteq(imageHist);
+                    
+    %binaryMask = imclearborder(binaryMask, 8);
+    binaryMask = bwareaopen(binaryMask, maxIslandNumPixels);
+    
+    close all
+    imshow(imageHist)
+    figure
+    imshow(inputImage)
+    figure
+    imshow(imageHist)
+    figure
+    imshow(binaryMask)
+end
